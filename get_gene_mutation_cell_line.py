8ea4cf6099a3eb312a302a0012c68ca1531825fa
namd5 <drasko.tomic@yahoo.com>
2021-03-23 08:14:03 +0100

    update

--- get_gene_mutation_cell_line.py
+++ get_gene_mutation_cell_line.py
@@ -10,7 +10,7 @@ import requests
 t0 = time.time()
 # this token has to be manually obtained from https://cancer.sanger.ac.uk/cosmic/download
 
-TOKEN_NUMBER = "822239706399298093099245485975646525"
+TOKEN_NUMBER = "371818745204792537942947923028087475"
 WORKING_DIR_MUTATIONS = os.path.join(os.path.realpath('.'), 'genes', 'mutations')
 WORKING_DIR_SEQUENCES = os.path.join(os.path.realpath('.'), 'genes', 'sequences')
 
