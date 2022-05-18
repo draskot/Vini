d1c4fa9f17eb20ca6f64b4cc68ec7f52f061b137
Draško Tomič <eudraskot@vglogin0002.vega.izum.si>
2022-05-18 14:31:35 +0200

    updated on 18052022

--- cosmicTools.py
+++ cosmicTools.py
@@ -4,7 +4,7 @@ import requests
 import time
 import csv
 
-TOKEN_NUMBER = "239032394361423299171819117764047478"
+TOKEN_NUMBER = "62356951098701806539879589599473005"
 WORKING_DIR = os.path.join(os.path.realpath('.'), 'genes')
 
 def mapUniprotIDtoCosmicID(UNIPROT_ID):
