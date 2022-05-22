5bd9c6db1620669f47a7dd44459039eef5ad40ca
namd5 <drasko.tomic@yahoo.com>
2022-05-22 18:17:23 +0200

    updated on 22052022

--- get_gene_expression_parallel.py
+++ get_gene_expression_parallel.py
@@ -13,7 +13,7 @@ import csv_splitter
 t0 = time.time()
 # this token has to be manually obtained from https://cancer.sanger.ac.uk/cosmic/download
 
-TOKEN_NUMBER = "62356951098701806539879589599473005"
+TOKEN_NUMBER = "474581185078798351543902735043315731"
 WORKING_DIR = os.path.join(os.path.realpath('.'), 'genes', 'expressions')
 
 def mapUniprotIDtoCosmicID(UNIPROT_ID):
--- get_gene_mutation.py
+++ get_gene_mutation.py
@@ -15,7 +15,7 @@ import getopt
 t0 = time.time()
 # this token has to be manually obtained from https://cancer.sanger.ac.uk/cosmic/download
 
-TOKEN_NUMBER = "62356951098701806539879589599473005"
+TOKEN_NUMBER = "474581185078798351543902735043315731"
 WORKING_DIR_MUTATIONS = os.path.join(os.path.realpath('.'), 'genes', 'mutations')
 WORKING_DIR_SEQUENCES = os.path.join(os.path.realpath('.'), 'genes', 'sequences')
 
