95280cd2e767619fd6809e6202598cb37a965032
Josip Mesaric <{ID}+{username}@users.noreply.github.com>
2022-07-25 21:24:09 +0200

    Fix generated

--- generateMutatedFASTAseqCellLine.py
+++ generateMutatedFASTAseqCellLine.py
@@ -50,21 +50,13 @@ def main(argv):
     mutations_df = filterMutations(mutation_file, genes_list)
     print mutations_df
     for gene in genes_list:
-<<<<<<< HEAD
         mutations = mutations_df[mutations_df['GENE_NAME'] == gene]
         if not mutations.empty:
             sequence_filename = os.path.join(WORKING_DIR_SEQUENCES, gene + '_sequence.csv')
             new_sequence = cosmicTools.applyMutationsToFASTA(mutations=mutations, FASTAfile=os.path.join(WORKING_DIR_SEQUENCES,sequence_filename))
-=======
-        print "gene: ", gene
-        mutations = mutations_df[mutations_df['GENE_NAME'] == gene]
-        if not mutations.empty:
-            sequence_filename = os.path.join(WORKING_DIR_SEQUENCES, gene + '_sequence.csv')
-            new_sequence = cosmicTools.applyMutationsToFASTA(mutations, os.path.join(WORKING_DIR_SEQUENCES,sequence_filename))
->>>>>>> 3fb980f609352583d5e3f93c0aff11c1810a96a6
             cosmicTools.saveSequenceToFASTA(gene, new_sequence, WORKING_DIR_SEQUENCES)
 
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main(sys.argv[1:]) 
