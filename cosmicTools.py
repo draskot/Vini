03ddf1e542a7bcd930e1851a7b96461bc36ea390
Draško Tomič <eudraskot@vglogin0004.vega.izum.si>
2023-08-29 09:31:47 +0200

    29082023 update

--- cosmicTools.py
+++ cosmicTools.py
@@ -7,7 +7,7 @@ import csv
 from time import sleep
 import random
 
-TOKEN_NUMBER = "652156455737727788622971581944036305"
+TOKEN_NUMBER = "2319488909516510976505187276388359"
 WORKING_DIR = os.path.join(os.path.realpath('.'), 'genes')
 
 def mapUniprotIDToCosmicID_fromList(UNIPROT_LIST):
--- get_gene_expression_parallel.py
+++ get_gene_expression_parallel.py
@@ -13,7 +13,7 @@ import csv_splitter
 t0 = time.time()
 # this token has to be manually obtained from https://cancer.sanger.ac.uk/cosmic/download
 
-TOKEN_NUMBER = "652156455737727788622971581944036305"
+TOKEN_NUMBER = "2319488909516510976505187276388359"
 WORKING_DIR = os.path.join(os.path.realpath('.'), 'genes', 'expressions')
 
 def mapUniprotIDtoCosmicID(UNIPROT_ID):
