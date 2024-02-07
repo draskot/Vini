fa7dc6f71e001df435ec096bbe89a9c366431fed
Draško Tomič <eudraskot@vglogin0003.vega.izum.si>
2024-02-07 12:57:14 +0100

    updated Rosetta

--- cosmicTools.py
+++ cosmicTools.py
@@ -7,7 +7,7 @@ import csv
 from time import sleep
 import random
 
-TOKEN_NUMBER = "673346272736953839741393480663958125"
+TOKEN_NUMBER = "794018082630551578840299459608569607"
 WORKING_DIR = os.path.join(os.path.realpath('.'), 'genes')
 
 def mapUniprotIDToCosmicID_fromList(UNIPROT_LIST):
--- get_gene_mutation_cell_line.py
+++ get_gene_mutation_cell_line.py
@@ -10,7 +10,7 @@ import requests
 t0 = time.time()
 # this token has to be manually obtained from https://cancer.sanger.ac.uk/cosmic/download
 
-TOKEN_NUMBER = "673346272736953839741393480663958125"
+TOKEN_NUMBER = "794018082630551578840299459608569607"
 WORKING_DIR_MUTATIONS = os.path.join(os.path.realpath('.'), 'genes', 'mutations')
 WORKING_DIR_SEQUENCES = os.path.join(os.path.realpath('.'), 'genes', 'sequences')
 
